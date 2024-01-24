98122dc81c7b926fb84d3db5e775bd36671b47ae
juanjose85 <juanjosesebastian@hotmail.com>
2024-01-24 15:25:27 -0600

    Se agrego correccion al tipo de dato para el puerto

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
+      port: Number(process.env.MYSQL_PORT),
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PWD,
       database: 'blog',
